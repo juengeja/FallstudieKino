a0619225a1a3aa600c952c3ea2a09ee52d3bb535
LeonEngelhardt <engelhardt.leon2000@gmx.de>
2021-09-29 18:32:00 +0200

    Booking hinzugefügt

---
+++ Frontend/src/components/Seats.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './BookMySeats.css';
+
+const Seats = (props) => {
+
+    return (
+        // <div className={classes.section}>
+        <div className="section">
+            {props.values.map(seat => {
+                const isAvailable = props.availableSeats.includes(seat);
+                const isBooked = props.bookedSeats.includes(seat);
+                let seatClass;
+                if(!isAvailable) {
+                    seatClass = classes.disabled;
+                }
+                if(isBooked) {
+                    seatClass = classes.booked;
+                }
+                return <div className="section" onClick={props.addSeat} key={seat}>{seat}</div>;
+                //return <div className={seatClass} onClick={props.addSeat} key={seat}>{seat}</div>;
+            })}
+        </div>
+    );
+}
+export default Seats;
