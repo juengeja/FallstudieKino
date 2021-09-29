7bc6f08ceb2996708d91313d7320cbaba0d2993c
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2021-09-29 16:16:29 +0200

    Adding Backend Struckture

---
+++ Backend/src/main/java/com/example/kinoticketreservierungssystem/KinoticketreservierungssystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinoticketreservierungssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoticketreservierungssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoticketreservierungssystemApplication.class, args);
+    }
+
+}
