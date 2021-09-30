82c089cf32d6d6993bb2192d05f3c7b2ea4afbb0
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2021-09-30 19:20:49 +0200

    Movie Controller with Mock-DB

--- Backend/src/main/java/com/example/kinoticketreservierungssystem/controller/MovieController.java
+++ Backend/src/main/java/com/example/kinoticketreservierungssystem/controller/MovieController.java
@@ -10,21 +10,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/movies")
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/api")
 @RestController
 @CrossOrigin(origins = "*")
 public class MovieController {
 
     private final MovieRepository movieRepository;
+    private static List<Movie> movies = new ArrayList<>();
 
     public MovieController(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
-
-    @GetMapping("/getAll")
+    @GetMapping("/moviedata")
     public ResponseEntity<Iterable<Movie>> getAllMovies(
     ) {
-        return new ResponseEntity<>(movieRepository.findAll(), HttpStatus.OK);
+        Movie movie = new Movie();
+        movie.setName("James Bond");
+        movie.setDuration(162);
+        movie.setDescription("Beschreibung des Films James Bond");
+        movie.setGenre("Action");
+
+        movies.add(movie);
+
+        return new ResponseEntity<>(movies, HttpStatus.OK);
     }
 }
