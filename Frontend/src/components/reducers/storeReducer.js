ebd9cfd3e7cb497647f0ded20e5158d1f74b65f2
Tobias Fitzke <tobias.fitzke@dxc.com>
2021-10-31 19:42:46 +0100

    fix

--- Frontend/src/components/reducers/storeReducer.js
+++ Frontend/src/components/reducers/storeReducer.js
@@ -4,8 +4,7 @@ import { ADD_TO_CART,ADD_ITEM,REMOVE_ITEM,REMOVE_ALL,CHANGE_STATE } from '../act
 const initState = {
     items: [],
     addedItems:[],
-    //TEMP auf true
-    loginState: true
+    loginState: false
 }
 const cartReducer= (state = initState,action)=>{
     //INSIDE HOME COMPONENT
